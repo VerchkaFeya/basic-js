084757c3ca17bc0ddbbcc5e466505b2f1cfbad5e
Vera Krasnova <azirafail@mail.ru>
2022-04-28 13:11:39 +0300

    complete transform array tsk

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!(arr instanceof Array)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  }
+
+  if (arr.length == 0) {
+    return arr;
+  }
+
+
+  let flagsArray = arr.filter(item => typeof item === 'string');
+
+  for (let i = 0; i < flagsArray.length; i++) {
+      let index = arr.indexOf(flagsArray[i]);
+
+      if (flagsArray[i] === `--discard-next`) { // если надо удалить следующий элемент `--discard-next`
+        arr[index + 1] = undefined;
+        let firstSlice = arr.slice(0, index);
+        let secondSlice = arr.slice(index+1);
+        arr = [...firstSlice, ...secondSlice];
+      } else if (flagsArray[i] === `--discard-prev`) { // если надо удалить предыдущий элемент `--discard-prev`
+        arr[index - 1] = undefined;
+        let firstSlice = arr.slice(0, index);
+        let secondSlice = arr.slice(index+1);
+        arr = [...firstSlice, ...secondSlice];
+      } else if (flagsArray[i] === `--double-next`) {  // если надо продублировать последующий элемент `--double-next`
+        let firstSlice = arr.slice(0, index);
+        let secondSlice = arr.slice(index + 1);
+        firstSlice.push(secondSlice[0]);
+        arr = [...firstSlice, ...secondSlice];
+      } else if (flagsArray[i] === `--double-prev`) { // если надо продублировать предыдущий элемент `--double-prev`
+        let firstSlice = arr.slice(0, index);
+        let secondSlice = arr.slice(index + 1);
+        firstSlice.push(firstSlice[index-1]);
+        arr = [...firstSlice, ...secondSlice];
+      }
+    }
+    return arr.filter(item => item != undefined);
 }
 
 module.exports = {
